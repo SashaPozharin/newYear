2004bea21fb0e14be542aa29fac7558dbc0c83f0
SashaPozharin <68804550+SashaPozharin@users.noreply.github.com>
2020-12-13 15:09:56 +0300

    ypa

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let music;
 
 $(document).ready(function (){
     music = new Howl({
-        src:["https://ruv.hotmo.org/get/cuts/a8/0c/a80cb0f4f75a49ac7dfc2696be0b6e98/48261645/Christmas_Music_Christmas_Songs_Jingle_Bells_Christmas_Hits_Collective_-_Jingle_Bell_Rock_b128f0d132.mp3"],
+        src:["audio/muzon.mp3"],
         loop: true,
         volume: 0.45
     });
